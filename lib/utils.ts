1edd78bcbb8f233d9d19b03755c1edb3fe27e931
Avinash Vyas <avinash.vyas@ibm.com>
2024-10-08 11:49:59 +0530

    changes

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
-//  eslint-disable prefer-const 
-//  eslint-disable no-prototype-builtins 
+/* eslint-disable prefer-const */
+/* eslint-disable no-prototype-builtins */
 /*@typescript-eslint/no-explicit-any*/
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
